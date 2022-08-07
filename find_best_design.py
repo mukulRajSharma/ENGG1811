cf4d7cad9de84cd88dad4978ba6032b122465f88
mukul sharma <mukulsharma@pop-os.localdomain>
2022-08-07 21:16:28 +1000

    done

--- find_best_design.py
+++ find_best_design.py
@@ -11,9 +11,8 @@ import numpy as np
 
 def find_best_design(sway_array,B_array,K_array):
     
-
-
-
+    B_chosen = B_array[np.where(sway_array == np.min(sway_array))[0][0]]
+    K_chosen = K_array[np.where(sway_array == np.min(sway_array))[1][0]]
     return B_chosen, K_chosen 
 
 
